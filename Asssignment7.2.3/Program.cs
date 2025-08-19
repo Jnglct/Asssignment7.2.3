2488bc22b02cbe1b2df79507066a89f6abeba89b
Jnglct <rogerscole99@gmail.com>
2025-08-19 15:27:12 -0500

    Add project files.

---
+++ Asssignment7.2.3/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Asssignment7._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsAnagram("anagram", "nagaram"));
+        }
+        static bool IsAnagram(string s, string t)
+        {
+            int count = 0;
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+            foreach (char sitem in s)
+            {
+                foreach (char titem in t)
+                {
+                    if (sitem == titem)
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            if (count == s.Length)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
